d9ce1bcbc1aa2aca921101b822be493d648306af
Joseph Teichman <josteich@gmail.com>
2019-03-12 02:20:02 -0400

    Set up typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
   output: {
     path: __dirname + '/dist',
     filename: 'index_bundle.js'
