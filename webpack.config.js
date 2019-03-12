d5fb80af10ac5e4e959758db58e5b17ca2d9fea8
Joseph Teichman <jteich3@gmail.com>
2019-03-12 01:03:31 -0400

    Further setup

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({      
+      title: 'netrig',
+    })
+  ]
+}
