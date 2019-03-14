8ffefbdf92563bd94a61ba43360ec5923ad76f0a
Joseph Teichman <josteich@gmail.com>
2019-03-14 14:50:06 -0400

    First test prep

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
 import ReceiveAudio from "./ReceiveAudio";
 
-export const App = () => <div>
-    <h1>Hello there zzzz!</h1>
-    <ReceiveAudio url="zzz"></ReceiveAudio>
-</div>;
+export const App = () => {
+    let host = window.location.host;
+    return <div>
+        <h1>Hello there zzzz!</h1>
+        <ReceiveAudio url="ws://{{host}}/radio/audioOut"></ReceiveAudio>
+    </div>
+};
--- src/components/ReceiveAudio/index.tsx
+++ src/components/ReceiveAudio/index.tsx
@@ -1,10 +1,39 @@
 import * as React from "react";
 
 export interface ReceiveAudioProps { url: string; }
+interface AudioStreamConfig {samplesPerSecond: number; bitsPerSample: number;}
 
 class ReceiveAudio extends React.Component<ReceiveAudioProps, any> {
+    connection: WebSocket;
+    audioStreamConfig: AudioStreamConfig;
+
+    constructor(props: ReceiveAudioProps) {
+        super(props);
+        this.initiateConnection();
+    };
+
+    initiateConnection(){
+        let connection = new WebSocket(this.props.url);
+        //connection.binaryType
+        connection.onopen = () => {console.log("ReceiveAutdio connected");};
+        connection.onclose = () => {console.log("ReceiveAutdio connection closed");};
+        connection.onmessage = this.initiateAudio;
+
+        this.connection = connection;
+    };
+
+    initiateAudio(msg: MessageEvent){
+        console.log("WebSocket message received:", msg);
+        console.log(msg);
+        this.connection.onmessage = this.pushAudioOut;
+    }
+
+    pushAudioOut(msg: MessageEvent){
+        //todo
+    };
+
     render() {
         return <div>audio</div>;
-    }
+    };
 }
 export default ReceiveAudio;
