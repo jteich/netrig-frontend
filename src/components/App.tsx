8c64394f0adf693c258f4c5fd1172558351141ff
Joseph Teichman <josteich@gmail.com>
2019-03-14 16:28:56 -0400

    Fixed string interpolation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,6 @@ export const App = () => {
     let host = window.location.host;
     return <div>
         <h1>Hello there zzzz!</h1>
-        <ReceiveAudio url="ws://{{host}}/radio/audioOut"></ReceiveAudio>
+        <ReceiveAudio url={"ws://" + host + "/radio/audioOut"}></ReceiveAudio>
     </div>
 };
